7194d9bf56c84fdd8fd0d585d2c2bb8771db8bcc
fkkmemi <fkkmemi@gmail.com>
2020-05-04 19:50:00 +0900

    11 navigation drawer 만들기

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon/>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title>Page title</v-toolbar-title>
       <v-spacer/>
       <v-btn icon to="/about">
@@ -15,6 +15,20 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+    </v-navigation-drawer>
     <v-content>
       <router-view/>
     </v-content>
@@ -27,6 +41,11 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
