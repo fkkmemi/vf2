610a8104bc01ed587b189ad060f7319e3725ebd2
fkkmemi <fkkmemi@gmail.com>
2020-05-08 21:20:39 +0900

    17 제목 수정해보기

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <v-app-bar-nav-icon @click="drawer = !drawer" />
-      <site-title :title="title"></site-title>
+      <site-title :title="site.title"></site-title>
       <v-spacer/>
       <v-btn icon @click="save"><v-icon>mdi-check</v-icon></v-btn>
       <v-btn icon @click="read"><v-icon>mdi-numeric</v-icon></v-btn>
       <v-btn icon @click="readOne"><v-icon>mdi-account-badge-alert</v-icon></v-btn>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer">
-      <site-menu></site-menu>
+      <site-menu :items="site.menu"></site-menu>
     </v-navigation-drawer>
     <v-content>
       <router-view/>
     </v-content>
-    <site-footer :footer="footer"></site-footer>
+    <site-footer :footer="site.footer"></site-footer>
   </v-app>
 </template>
 
@@ -29,15 +29,28 @@ export default {
   data () {
     return {
       drawer: false,
-      items: [],
-      title: '나의 타이틀입니다',
-      footer: '푸터입니다'
+      site: {
+        menu: [],
+        title: '나의 타이틀입니다',
+        footer: '푸터입니다'
+      }
     }
   },
-  mounted () {
-    console.log(this.$firebase)
+  created () {
+    this.subscribe()
   },
   methods: {
+    subscribe () {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
+        const v = sn.val()
+        if (!v) {
+          this.$firebase.database().ref().child('site').set(this.site)
+        }
+        this.site = v
+      }, (e) => {
+        console.log(e.message)
+      })
+    },
     save () {
       console.log('save@@@')
       this.$firebase.database().ref().child('abcd').child('abcd').child('abcd').set({
--- src/views/site/title.vue
+++ src/views/site/title.vue
@@ -1,11 +1,44 @@
 <template>
   <v-toolbar-title>
     {{ title }}
-
+    <v-btn icon @click="openDialog"><v-icon>mdi-grease-pencil</v-icon></v-btn>
+    <v-dialog v-model="dialog" max-width="400">
+      <v-card>
+        <v-card-title>
+          제목 수정
+          <v-spacer/>
+          <v-btn icon @click="save"><v-icon>mdi-content-save</v-icon></v-btn>
+          <v-btn icon @click="dialog=false"><v-icon>mdi-close</v-icon></v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field v-model="text" outlined label="제목" @keypress.enter="save" hide-details />
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-toolbar-title>
 </template>
 <script>
 export default {
-  props: ['title']
+  props: ['title'],
+  data () {
+    return {
+      dialog: false,
+      text: this.title
+    }
+  },
+  methods: {
+    openDialog () {
+      this.dialog = true
+    },
+    async save () {
+      try {
+        await this.$firebase.database().ref().child('site').update({ title: this.text })
+      } catch (e) {
+        console.log(e.message)
+      } finally {
+        this.dialog = false
+      }
+    }
+  }
 }
 </script>
