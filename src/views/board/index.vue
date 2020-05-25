d128ce43daf2b4103a1edb0941ded4442b37e31c
fkkmemi <fkkmemi@gmail.com>
2020-05-25 21:40:16 +0900

    27 firestore 수정, 지우기

--- src/views/board/index.vue
+++ src/views/board/index.vue
@@ -1,20 +1,30 @@
 <template>
   <v-card>
     <v-card-title>board test</v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+    >
+      <template v-slot:item.id="{ item }">
+        <v-btn icon @click="openDialog(item)"><v-icon>mdi-pencil</v-icon></v-btn>
+        <v-btn icon @click="remove(item)"><v-icon>mdi-delete</v-icon></v-btn>
+      </template>
+    </v-data-table>
     <v-card-actions>
       <v-btn @click="read"><v-icon left>mdi-page-next</v-icon></v-btn>
-      <v-btn @click="openDialog"><v-icon left>mdi-pencil</v-icon></v-btn>
+      <v-btn @click="openDialog(null)"><v-icon left>mdi-pencil</v-icon></v-btn>
     </v-card-actions>
     <v-dialog max-width="500" v-model="dialog">
       <v-card>
         <v-form>
           <v-card-text>
-              <v-text-field v-model="form.title"></v-text-field>
-              <v-text-field v-model="form.content"></v-text-field>
+            <v-text-field v-model="form.title"></v-text-field>
+            <v-text-field v-model="form.content"></v-text-field>
           </v-card-text>
           <v-card-actions>
             <v-spacer/>
-            <v-btn @click="save">save</v-btn>
+            <v-btn @click="update" v-if="selectedItem">save</v-btn>
+            <v-btn @click="add" v-else>save</v-btn>
           </v-card-actions>
         </v-form>
       </v-card>
@@ -25,20 +35,41 @@
 export default {
   data () {
     return {
+      headers: [
+        { value: 'title', text: '제목' },
+        { value: 'content', text: '내용' },
+        { value: 'id', text: 'id' }
+      ],
       items: [],
       form: {
         title: '',
         content: ''
       },
-      dialog: false
+      dialog: false,
+      selectedItem: null
     }
   },
+  created () {
+    this.read()
+  },
   methods: {
-    openDialog () {
+    openDialog (item) {
+      this.selectedItem = item
       this.dialog = true
+      if (!item) {
+        this.form.title = ''
+        this.form.content = ''
+      } else {
+        this.form.title = item.title
+        this.form.content = item.content
+      }
     },
-    save () {
+    add () {
       this.$firebase.firestore().collection('boards').add(this.form)
+      this.dialog = false
+    },
+    update () {
+      this.$firebase.firestore().collection('boards').doc(this.selectedItem.id).update(this.form)
       this.dialog = false
     },
     async read () {
@@ -50,7 +81,10 @@ export default {
           id: v.id, title: item.title, content: item.content
         }
       })
-      console.log(this.items)
+      // console.log(this.items)
+    },
+    remove (item) {
+      this.$firebase.firestore().collection('boards').doc(item.id).delete()
     }
 
   }
