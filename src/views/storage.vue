dc84544b084bc8f0e50f6b271df348ab19493c50
fkkmemi <fkkmemi@gmail.com>
2020-06-11 21:33:12 +0900

    34 axios로 storage 데이터 가져오기

--- src/views/storage.vue
+++ src/views/storage.vue
@@ -1,21 +1,32 @@
 <template>
   <v-sheet>
-    sss
+    <v-textarea v-model="text">
+
+    </v-textarea>
+    <v-btn @click="read">read</v-btn>
     <v-btn @click="write">write</v-btn>
   </v-sheet>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
-      text: ''
+      text: '',
+      url: ''
     }
   },
   methods: {
     async write () {
       const sn = await this.$firebase.storage().ref().child('xxx.text').putString('abcd')
       const url = await sn.ref.getDownloadURL()
-      console.log(url)
+      this.url = url
+    },
+    async read () {
+      const r = await axios.get(this.url)
+      console.log(r)
+      this.text = r.data
     }
   }
 }
