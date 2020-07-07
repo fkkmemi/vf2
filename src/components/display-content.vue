10f49b9d775f82a30021c01e60b2223faa83fab1
fkkmemi <fkkmemi@gmail.com>
2020-07-07 20:34:45 +0900

    45 내용 표시하는 창 만들기

---
+++ src/components/display-content.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card>
+    <v-toolbar color="primary" dark>
+      <v-toolbar-title>
+        {{item.title}}
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-btn @click="$emit('close')" icon><v-icon>mdi-close</v-icon></v-btn>
+    </v-toolbar>
+    <v-card-text>
+      {{item.url}}
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  props: ['item'],
+  mounted () {
+    console.log('mounted')
+  }
+}
+</script>
--- src/views/board/article/index.vue
+++ src/views/board/article/index.vue
@@ -1,32 +1,41 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="items"
-    :server-items-length="info.count"
-    :options.sync="options"
-    :items-per-page="5"
-    :footer-props="{
-      'items-per-page-options':[5, 10, 20, 30, 50],
-    }"
-    must-sort
-    item-key="id"
-  >
-    <template v-slot:item.createdAt="{item}">
-      <display-time :time="item.createdAt"></display-time>
-    </template>
-    <template v-slot:item.user.displayName="{item}">
-      <display-user :user="item.user"></display-user>
-    </template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :server-items-length="info.count"
+      :options.sync="options"
+      :items-per-page="5"
+      :footer-props="{
+        'items-per-page-options':[5, 10, 20, 30, 50],
+      }"
+      must-sort
+      item-key="id"
+    >
+      <template v-slot:item.createdAt="{item}">
+        <display-time :time="item.createdAt"></display-time>
+      </template>
+      <template v-slot:item.title="{item}">
+        <a @click="openDialog(item)">{{item.title}}</a>
+      </template>
+      <template v-slot:item.user.displayName="{item}">
+        <display-user :user="item.user"></display-user>
+      </template>
 
-  </v-data-table>
+    </v-data-table>
+    <v-dialog v-if="selectedItem" v-model="dialog">
+      <display-content :item="selectedItem" @close="dialog=false"></display-content>
+    </v-dialog>
+  </div>
 </template>
 <script>
 import { head, last } from 'lodash'
 import DisplayTime from '@/components/display-time'
 import DisplayUser from '@/components/display-user'
+import DisplayContent from '@/components/display-content'
 
 export default {
-  components: { DisplayTime, DisplayUser },
+  components: { DisplayTime, DisplayUser, DisplayContent },
   props: ['info', 'document'],
   data () {
     return {
@@ -43,7 +52,9 @@ export default {
         sortBy: ['createdAt'],
         sortDesc: [true]
       },
-      docs: []
+      docs: [],
+      dialog: false,
+      selectedItem: null
     }
   },
   watch: {
@@ -65,6 +76,9 @@ export default {
         this.subscribe(arrow)
       },
       deep: true
+    },
+    dialog (n) {
+      if (!n) this.selectedItem = null
     }
   },
   created () {
@@ -104,6 +118,10 @@ export default {
           return item
         })
       })
+    },
+    openDialog (item) {
+      this.selectedItem = item
+      this.dialog = true
     }
 
   }
