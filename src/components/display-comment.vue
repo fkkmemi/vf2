531f65f695b26c9da739b269df9f4b9f514f0b88
fkkmemi <fkkmemi@gmail.com>
2020-07-13 23:37:52 +0900

    50 댓글 확인하기

--- src/components/display-comment.vue
+++ src/components/display-comment.vue
@@ -3,14 +3,39 @@
     <v-card-title>
       <v-text-field v-model="comment" outlined label="댓글 작성" @keypress.enter="save" hide-details></v-text-field>
     </v-card-title>
+    <template v-for="(item, i) in items">
+      <v-list-item :key="item.id">
+        <v-list-item-action>
+          <display-user :user="item.user"></display-user>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-subtitle v-text="item.comment"></v-list-item-subtitle>
+          <v-list-item-subtitle>
+            <display-time :time="item.createdAt"></display-time>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider :key="i"></v-divider>
+    </template>
+    <v-list-item>
+      <v-btn @click="more" text color="primary" block>더보기</v-btn>
+    </v-list-item>
   </v-card>
 </template>
 <script>
+
+import DisplayTime from '@/components/display-time'
+import DisplayUser from '@/components/display-user'
+
 export default {
+  components: { DisplayTime, DisplayUser },
   props: ['docRef'],
   data () {
     return {
-      comment: ''
+      comment: '',
+      items: [],
+      unsubscribe: null,
+      limit: 5
     }
   },
   computed: {
@@ -18,7 +43,29 @@ export default {
       return this.$store.state.user
     }
   },
+  created () {
+    this.subscribe()
+  },
+  destroyed () {
+    if (this.unsubscribe) this.unsubscribe()
+  },
   methods: {
+    subscribe () {
+      if (this.unsubscribe) this.unsubscribe()
+      this.unsubscribe = this.docRef.collection('comments').limit(this.limit).onSnapshot(sn => {
+        if (sn.empty) {
+          this.items = []
+          return
+        }
+        this.items = sn.docs.map(doc => {
+          const item = doc.data()
+          item.id = doc.id
+          item.createdAt = item.createdAt.toDate()
+          item.updatedAt = item.updatedAt.toDate()
+          return item
+        })
+      })
+    },
     async save () {
       const doc = {
         createdAt: new Date(),
@@ -37,6 +84,10 @@ export default {
       batch.set(this.docRef.collection('comments').doc(id), doc)
       await batch.commit()
       this.comment = ''
+    },
+    more () {
+      this.limit += 5
+      this.subscribe()
     }
   }
 }
