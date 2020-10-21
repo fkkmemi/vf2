f827665c4e541063e7d6826274da12136ada1ee9
fkkmemi <fkkmemi@gmail.com>
2020-10-21 19:49:12 +0900

    89 메인페이지 만들기

--- src/components/board/form.vue
+++ src/components/board/form.vue
@@ -1,204 +1,256 @@
 <template>
-  <v-container v-if="!loaded" fluid>
-    <v-skeleton-loader type="card"></v-skeleton-loader>
+  <v-container fluid v-if="!loaded">
+    <v-skeleton-loader type="article"></v-skeleton-loader>
   </v-container>
-  <v-container v-else-if="loaded && !user || (user && user.level > 0)" fluid>
+  <v-container fluid v-else-if="loaded && !board">
     <v-alert type="warning" border="left" class="mb-0">
-      게시판 생성은 관리자만 할 수 있습니다
+      게시판 정보를 불러오지 못했습니다
     </v-alert>
   </v-container>
   <v-container v-else fluid :class="$vuetify.breakpoint.xs ? 'pa-0' : ''">
     <v-form>
       <v-card :loading="loading" outlined :tile="$vuetify.breakpoint.xs">
         <v-toolbar color="transparent" dense flat>
-          <v-toolbar-title>게시판 정보 작성</v-toolbar-title>
-        <v-spacer/>
-        <v-btn icon @click="save" :disabled="user && user.level !== 0"><v-icon>mdi-content-save</v-icon></v-btn>
-        <v-btn icon @click="$router.push('/board/' + boardId)"><v-icon>mdi-close</v-icon></v-btn>
+          <v-toolbar-title>게시물 작성</v-toolbar-title>
+          <v-spacer/>
+          <v-btn icon @click="save" :disabled="!user"><v-icon>mdi-content-save</v-icon></v-btn>
+          <v-btn icon @click="$router.push('/board/' + boardId)"><v-icon>mdi-close</v-icon></v-btn>
         </v-toolbar>
         <v-divider/>
         <v-card-text>
-          <v-text-field v-model="form.category" outlined label="종류"></v-text-field>
-          <v-text-field v-model="form.title" outlined label="제목"></v-text-field>
-          <v-textarea v-model="form.description" outlined label="설명" hide-details></v-textarea>
-        </v-card-text>
-        <v-card-text>
-          <v-card outlined>
-            <v-subheader>등록된 종류</v-subheader>
-            <v-card-text>
-              <v-chip
-                color="info"
-                label
-                small
-                v-for="(item, i) in form.categories"
-                :key="i"
-                class="mr-2 mb-2">
-                {{item}} <v-icon small right @click="removeCategory(item, i)">mdi-close</v-icon>
-                </v-chip>
-            </v-card-text>
-            <v-card-actions>
-              <div width="100">
-                <v-text-field
-                  v-model="category"
-                  append-icon="mdi-plus"
-                  label="등록"
-                  placeholder="eg) social"
-                  hide-details
-                  outlined
-                  dense
-                  @click:append="saveCategory"
-                  @keypress.enter="saveCategory"
-                />
-              </div>
-            </v-card-actions>
-          </v-card>
-        </v-card-text>
-        <v-card-text>
-          <v-card outlined>
-            <v-subheader>등록된 태그</v-subheader>
-            <v-card-text>
-              <v-chip
-                color="info"
-                label
-                small
+          <v-row>
+            <v-col cols="12" sm="2" v-if="board">
+              <v-select
+                v-model="form.important"
+                :items="[
+                  { value: 0, text: '일반'},
+                  { value: 1, text: '공지'},
+                  { value: 2, text: '중요'}
+                ]"
+                label="유형"
+                outlined hide-details />
+            </v-col>
+            <v-col cols="12" sm="4" v-if="board">
+              <v-combobox
+                v-model="form.category"
+                :items="board.categories"
+                label="종류"
+                outlined hide-details />
+            </v-col>
+            <v-col cols="12" sm="6" v-if="board">
+              <v-combobox
+                v-model="form.tags"
+                :items="board.tags"
+                label="태그"
                 outlined
-                v-for="(item, i) in form.tags"
-                :key="i"
-                class="mr-2 mb-2">
-                {{item}} <v-icon small right @click="removeTag(item, i)">mdi-close</v-icon>
-              </v-chip>
-            </v-card-text>
-            <v-card-actions>
-              <div width="100">
-                <v-text-field
-                  v-model="tag"
-                  append-icon="mdi-plus"
-                  label="등록"
-                  placeholder="eg) vuetify"
-                  hide-details
-                  outlined
-                  dense
-                  @click:append="saveTag"
-                  @keypress.enter="saveTag"
-                />
-              </div>
-
-            </v-card-actions>
-          </v-card>
+                multiple
+                small-chips hide-details />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="form.title" outlined label="제목" hide-details></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <editor
+                v-if="!exists"
+                :initialValue="form.content"
+                ref="editor" initialEditType="wysiwyg" height="400px"
+                :options="options"
+                ></editor>
+              <template v-else>
+                <editor
+                  v-if="form.content"
+                  :initialValue="form.content"
+                  ref="editor" initialEditType="wysiwyg" height="400px"
+                  :options="options"></editor>
+                <v-container v-else>
+                  <v-row justify="center" align="center">
+                    <v-progress-circular indeterminate></v-progress-circular>
+                  </v-row>
+                </v-container>
+              </template>
+            </v-col>
+          </v-row>
         </v-card-text>
+        <v-divider/>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn text @click="$router.push('/board/' + boardId)"><v-icon left>mdi-close</v-icon>취소</v-btn>
+          <v-btn @click="save" :disabled="!user" text color="primary">
+            <v-icon left>mdi-content-save</v-icon> 저장
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-form>
   </v-container>
 </template>
 <script>
+import axios from 'axios'
+import getSummary from '@/util/getSummary'
+import imageCompress from '@/util/imageCompress'
+
 export default {
-  props: ['boardId', 'action'],
+  props: ['boardId', 'articleId', 'action'],
   data () {
     return {
       form: {
         category: '',
+        tags: [],
         title: '',
-        description: '',
-        categories: [],
-        tags: []
+        content: '',
+        images: [],
+        important: 0
       },
       exists: false,
       loading: false,
       ref: null,
-      category: '',
-      tag: '',
-      loaded: false
+      article: null,
+      board: null,
+      loaded: false,
+      options: {
+        language: 'ko',
+        hooks: {
+          addImageBlobHook: this.addImageBlobHook
+        }
+      },
+      plugins: [
+        [this.youtubePlugin]
+
+      ]
     }
   },
   computed: {
     user () {
       return this.$store.state.user
+    },
+    fireUser () {
+      return this.$store.state.fireUser
     }
   },
   watch: {
     boardId () {
+      this.fetch()
+    },
+    articleId () {
+      this.fetch()
+    },
+    action () {
       this.fetch()
     }
   },
   created () {
     this.fetch()
   },
+  mounted () {
+  },
+  destroyed () {
+  },
   methods: {
     async fetch () {
       this.ref = this.$firebase.firestore().collection('boards').doc(this.boardId)
       this.loaded = false
-      const doc = await this.ref.get()
+      const docBoard = await this.ref.get()
       this.loaded = true
+      this.board = docBoard.data()
+      const doc = await this.ref.collection('articles').doc(this.articleId).get()
       this.exists = doc.exists
-      if (this.exists) {
-        const item = doc.data()
-        this.form.category = item.category
-        this.form.title = item.title
-        this.form.description = item.description
-        this.form.categories = item.categories
-        this.form.tags = item.tags
-      }
+      if (!this.exists) return
+      const item = doc.data()
+      this.article = item
+      this.form.title = item.title
+      this.form.category = item.category
+      this.form.tags = item.tags
+      this.form.images = item.images
+      if (!item.images) this.form.images = []
+      this.form.important = item.important
+      const { data } = await axios.get(item.url)
+      this.form.content = typeof data === 'string' ? data : data.toString()
     },
     async save () {
-      if (!this.$store.state.fireUser) throw Error('로그인이 필요합니다')
-      if (!this.form.category || !this.form.title) throw Error('종류 제목은 필수 항목입니다')
-      const form = {
-        category: this.form.category,
-        title: this.form.title,
-        description: this.form.description,
-        categories: this.form.categories,
-        tags: this.form.tags,
-        updatedAt: new Date()
-      }
+      if (!this.fireUser) throw Error('로그인이 필요합니다')
+      if (!this.form.category) throw Error('종류는 필수 항목입니다')
+      if (!this.form.title) throw Error('제목은 필수 항목입니다')
+      const md = this.$refs.editor.invoke('getMarkdown')
+      if (!md) throw Error('내용은 필수 항목입니다')
       this.loading = true
       try {
-        if (!this.exists) {
-          form.createdAt = new Date()
-          form.count = 0
-          form.uid = this.$store.state.fireUser.uid
-          form.user = {
-            email: this.$store.state.user.email,
-            photoURL: this.$store.state.user.photoURL,
-            displayName: this.$store.state.user.displayName
-          }
-          form.readCount = 0
-          form.commentCount = 0
-          form.likeCount = 0
-          // form.categories = ['일반']
-          // form.tags = ['vue', 'firebase']
-          await this.ref.set(form)
-        } else {
-          await this.ref.update(form)
+        const doc = {
+          title: this.form.title,
+          category: this.form.category,
+          tags: this.form.tags,
+          images: this.findImagesFromDoc(md, this.form.images), // this.form.images,
+          updatedAt: new Date(),
+          summary: getSummary(md, 300, 'data:image'),
+          important: this.form.important
         }
-        this.$router.push('/board/' + this.boardId)
+        if (!this.exists) {
+          const fn = this.articleId + '-' + this.fireUser.uid + '.md'
+          const sn = await this.$firebase.storage().ref().child('boards').child(this.boardId).child(fn).putString(md)
+          doc.url = await sn.ref.getDownloadURL()
+          doc.createdAt = new Date()
+          doc.commentCount = 0
+          doc.readCount = 0
+          doc.uid = this.$store.state.fireUser.uid
+          doc.user = {
+            email: this.user.email,
+            photoURL: this.user.photoURL,
+            displayName: this.user.displayName
+          }
+          doc.likeCount = 0
+          doc.likeUids = []
+          await this.ref.collection('articles').doc(this.articleId).set(doc)
+          this.exists = true
+          this.$router.push('/board/' + this.boardId)
+        } else {
+          const fn = this.articleId + '-' + this.article.uid + '.md'
+          await this.$firebase.storage().ref().child('boards').child(this.boardId).child(fn).putString(md)
+          await this.ref.collection('articles').doc(this.articleId).update(doc)
+          this.$router.push(this.$route.path)
+        }
       } finally {
         this.loading = false
       }
     },
-    saveCategory () {
-      if (this.category.length > 20) throw Error('문자 개수를 초과했습니다')
-      if (this.category === '전체') throw Error('전체는 사용 불가능합니다')
-      const exists = this.form.categories.includes(this.category)
-      if (exists) throw Error('사용되고 있는 종류입니다')
-      this.form.categories.push(this.category)
-      this.category = ''
+    findImagesFromDoc (md, images) {
+      const filteredImages = images.filter(image => {
+        return md.indexOf(image.url) >= 0
+      })
+      return filteredImages
     },
-    async removeCategory (item, i) {
-      const sn = await this.ref.collection('articles').where('category', '==', item).limit(1).get()
-      if (!sn.empty) throw Error('사용되고 있는 종류입니다')
-      this.form.categories.splice(i, 1)
+    async imageUpload (file) {
+      if (!this.fireUser) throw Error('로그인이 필요합니다')
+      const thumbnail = await imageCompress(file)
+      const image = {
+        size: file.size,
+        id: '',
+        url: '',
+        thumbSize: thumbnail.size,
+        thumbId: '',
+        thumbUrl: ''
+      }
+
+      image.id = new Date().getTime() + '-' + this.fireUser.uid + '-' + file.name
+      const sn = await this.$firebase.storage().ref()
+        .child('images').child('boards')
+        .child(this.boardId).child(this.articleId).child(image.id)
+        .put(file)
+      image.url = await sn.ref.getDownloadURL()
+
+      image.thumbId = new Date().getTime() + '-' + this.fireUser.uid + '-thumb-' + file.name
+      const snt = await this.$firebase.storage().ref()
+        .child('images').child('boards')
+        .child(this.boardId).child(this.articleId).child(image.thumbId)
+        .put(thumbnail)
+      image.thumbUrl = await snt.ref.getDownloadURL()
+
+      this.form.images.push(image)
+      return image
     },
-    saveTag () {
-      if (this.tag.length > 20) throw Error('문자 개수를 초과했습니다')
-      const exists = this.form.tags.includes(this.tag)
-      if (exists) throw Error('사용되고 있는 태그입니다')
-      this.form.tags.push(this.tag)
-      this.tag = ''
-    },
-    async removeTag (item, i) {
-      const sn = await this.ref.collection('articles').where('tags', 'array-contains', item).limit(1).get()
-      if (!sn.empty) throw Error('사용되고 있는 태그입니다')
-      this.form.tags.splice(i, 1)
+    addImageBlobHook (blob, callback) {
+      this.imageUpload(blob)
+        .then(image => {
+          callback(image.url, 'img')
+        })
+        .catch(console.error)
     }
   }
 }
