d2aa0454cdea41e9f8e0216e9abc23f8370bc18b
fkkmemi <fkkmemi@gmail.com>
2021-02-05 20:18:01 +0900

    104 권한별 메뉴 노출하기

--- src/views/site/footer.vue
+++ src/views/site/footer.vue
@@ -6,13 +6,13 @@
     <v-dialog v-model="dialog" max-width="400">
       <v-card>
         <v-card-title>
-          제목 수정
+          바닥글 수정
           <v-spacer/>
           <v-btn icon @click="save"><v-icon>mdi-content-save</v-icon></v-btn>
           <v-btn icon @click="dialog=false"><v-icon>mdi-close</v-icon></v-btn>
         </v-card-title>
         <v-card-text>
-          <v-text-field v-model="text" outlined label="제목" @keypress.enter="save" hide-details />
+          <v-text-field v-model="text" outlined label="바닥글" @keypress.enter="save" hide-details />
         </v-card-text>
       </v-card>
     </v-dialog>
